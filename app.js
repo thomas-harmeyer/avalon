2cbe6fa91f940ca10c0d566ef637e48abc3cac25
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-01-18 02:50:04 -0500

    Added in body parser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
+var bodyParser = require('body-parser')
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
@@ -16,12 +17,14 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
+
 app.use(express.json());
 app.use(
   express.urlencoded({
     extended: false,
   })
 );
+app.use(bodyParser);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
