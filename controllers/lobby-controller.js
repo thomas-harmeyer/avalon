d01fdb1bf31d0c2f3d16e173fc04b481835cc281
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-02-04 14:22:39 -0500

    added comments and better verify

--- controllers/lobby-controller.js
+++ controllers/lobby-controller.js
@@ -5,93 +5,109 @@ const verifyController = require("./verify-controller");
 const title = "Avalon";
 
 function loadLanding(req, res) {
+  // clear cookies
   res.clearCookie("code");
   res.clearCookie("username");
+
+  // render landing page
   res.render("landing", {
     title: title,
   });
 }
 
 function createLobby(req, res) {
+  // get username from body and generate a new code and verify them
   let username = req.body.username_copy;
   let code =
     Math.floor(Math.random() * Math.floor(10)).toString() +
     Math.floor(Math.random() * Math.floor(10)).toString() +
     Math.floor(Math.random() * Math.floor(10)).toString() +
     Math.floor(Math.random() * Math.floor(10)).toString();
+  if (!verifyController.verifyCredentials(username, code)) {
+    return;
+  }
+  // create a new empty game with given code and last update now
   mongoController.connectToDb().then((db) => db.collection("games").insertOne({
     code: code,
     updated: Date.now(),
     started: "false"
-  }).then(function (err, result) {
+  }).then((result) => {
     req.body.username = username;
     req.body.code = code;
     joinLobby(req, res);
   })).catch((err) => verifyController.onMongoDbException(err, res));
 }
 
+// adds user to lobby set cookies and joins lobby and handles errors
 function joinLobby(req, res) {
+  // get creds and verify them
   let username = req.body.username;
   let code = req.body.code;
-  mongoController.connectToDb().then((function (db) {
-    db.collection("games").updateOne({
-      code: code,
-    }, {
-      $addToSet: {
-        users: {
-          username: username,
-        },
+  if (!verifyController.verifyCredentials(username, code)) {
+    return;
+  }
+  // add user to game with given O.P.E.
+  mongoController.connectToDb().then(((db) => db.collection("games").updateOne({
+    code: code,
+  }, {
+    $addToSet: {
+      users: {
+        username: username,
       },
-    }).then(function (result) {
-      if (result.matchedCount === 0) {
-        res.render("landing", {
-          err: "Sorry that game does not exist",
-          title: "Avalon",
-        });
-      } else {
-        res.cookie("username", username, {
-          maxAge: timeController.hour(24),
-        });
-        res.cookie("code", code, {
-          maxAge: timeController.hour(24),
-        });
-        res.redirect("/games");
-      }
-    })
-  }));
+    },
+  }).then(function (result) {
+    if (result.matchedCount == 0) {
+      res.render("landing", {
+        err: "Sorry that game does not exist",
+        title: "Avalon",
+      });
+    } else {
+      res.cookie("username", username, {
+        maxAge: timeController.hour(24),
+      });
+      res.cookie("code", code, {
+        maxAge: timeController.hour(24),
+      });
+      loadLobby(req, res);
+    }
+  }))).catch((err) => verifyController.onMongoDbException(err, res));
 }
 
 function removeUser(req, res) {
+  // get user to delete from req and if it does not exist handle it
   let toDelete = JSON.parse(req.body.toDelete).username;
   if (!verifyController.verifyExists(toDelete)) {
     return;
   }
-  mongoController.connectToDb().then(function (db) {
-    let users = db.collection("games");
-    users.updateMany({
-      code: req.cookies.code,
-    }, {
-      $pull: {
-        users: {
-          username: toDelete
-        },
+
+  // update game with code and pull user with the toDelete username
+  mongoController.connectToDb().then((db) => db.collection("games").updateMany({
+    code: req.cookies.code,
+  }, {
+    $pull: {
+      users: {
+        username: toDelete
       },
-    });
-  });
+    },
+  }));
+  // redirect to lobby
   loadLobby(req, res);
 }
 
 function loadLobby(req, res) {
+  // get creds and verify them
   let username = req.cookies.username;
   let code = req.cookies.code;
-  verifyController.verifyCredentials(username, code);
-  mongoController.connectToDb().then((db) => {
-    db.collection("games").findOne({
-      code: code,
-    }).then((result) => res.render("games", {
-      games: result,
-    }));
-  }).catch((err) => verifyController.onMongoDbException(err, res));
+  if (!verifyController.verifyCredentials(username, code)) {
+    return;
+  }
+
+  // find the game you're in and render it
+  mongoController.connectToDb().then((db) => db.collection("games").findOne({
+    code: code,
+  }).then((result) => res.render("games", {
+    games: result,
+  }))).catch((err) => verifyController.onMongoDbException(err, res));
 }
 
 module.exports = {
