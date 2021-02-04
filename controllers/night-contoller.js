7ff20533d3ca1855fe02aa25c750c8f03511035d
Thomas Harmeyer <thomasmharmeyer@gmail.com>
2021-02-04 14:23:02 -0500

    refactored night controller to use promises

--- controllers/night-contoller.js
+++ controllers/night-contoller.js
@@ -1,156 +1,170 @@
-var timeController = require("../controllers/time-controller");
-var assert = require("assert");
-var mongoController = require("../controllers/mongo-controller");
+const timeController = require("./time-controller");
+const mongoController = require("./mongo-controller");
+const verifyController = require("./verify-controller");
 
+
+// check if your game has started
 function hasStarted(req, res) {
-  let code = req.cookies.code;
-  mongoController.connectToDb(function (db) {
-    db.collection("games").findOne({
-        code: code,
-      },
-      function (err, result) {
-        res.send(result.started == 'true');
+  // get creds and verify them
+  const username = req.cookies.code;
+  const code = req.cookies.code;
+  if (!verifyController.verifyCredentials(username, code)) {
+    return;
+  }
 
-
-      });
-  });
+  // return if a game has started given an O.P.E.
+  mongoController.connectToDb().then((db) => db.collection("games").findOne({
+    code: code,
+  }).then(
+    (result) => res.send(result.started == 'true')
+  )).catch((err) => verifyController.onMongoDbException(err, res));
 }
 
+// assign roles to everyone in your game
+// if the game has already started it will not assign roles and send you to night
 function assignRoles(req, res) {
-  let code = req.cookies.code;
-  let username = req.cookies.username;
-  var users;
-  mongoController.connectToDb(function (db) {
-    db.collection("games").findOne({
-        code: code,
-      },
-      function (err, result) {
-        if (result.started == 'true') {
-          loadNight(req, res);
-          return;
-        }
-        assert.strictEqual(null, err);
-        users = result.users;
-        let bulkUpdate = [];
-        let roles = getRoles(users.length);
-        users.forEach((value, key) => {
-          let username = value.username;
-          bulkUpdate.push({
-            updateOne: {
-              filter: {
-                code: code,
-                "users.username": username,
-              },
-              update: {
-                $set: {
-                  "users.$.role": roles.pop(),
-                  started: "true"
-                },
-              },
+  // get creds and verify them
+  const username = req.cookies.code;
+  const code = req.cookies.code;
+  if (!verifyController.verifyCredentials(username, code)) {
+    return;
+  }
+
+  // find the game given an O.P.E.
+  mongoController.connectToDb().then((db) => db.collection("games").findOne({
+    code: code,
+  }).then((result) => {
+    // if the game has started then don't assign roles instead go to night
+    if (result.started == 'true') {
+      loadNight(req, res);
+      return;
+    }
+
+    // otherwise let users be the list of users in the game
+    const users = result.users;
+    let bulkUpdate = [];
+    let roles = getRoles(users.length);
+
+    // make the req used to update all users
+    users.forEach((value, key) => {
+      let username = value.username;
+      bulkUpdate.push({
+        updateOne: {
+          filter: {
+            code: code,
+            "users.username": username,
+          },
+          update: {
+            $set: {
+              "users.$.role": roles.pop(),
+              started: "true"
             },
-          });
-        });
-        mongoController.connectToDb((db) => {
-          let collection = db.collection("games");
-          collection.bulkWrite(bulkUpdate, (err, result) => {
-            loadNight(req, res);
-          });
-        });
-      }
-    );
-  });
+          },
+        },
+      });
+    });
+
+    //write the bulkUpdate then load night
+    mongoController.connectToDb().then((db) => db.collection("games").bulkWrite(bulkUpdate).then(() => loadNight(req, res)))
+  }));
 }
 
+// loads night; finds your role and the other players you role should know
 function loadNight(req, res) {
-  let code = req.cookies.code;
-  let username = req.cookies.username;
-  mongoController.connectToDb((db) => {
-    let collection = db.collection("games");
-    collection.findOne({
-        code: code,
+  // get creds and verify them
+  const username = req.cookies.code;
+  const code = req.cookies.code;
+  if (!verifyController.verifyCredentials(username, code)) {
+    return;
+  }
 
-      },
-      (err, result) => {
-        let users = "";
-        if (result.users)
-          users = result.users;
-        else {
-          res.redirect("/");
-          return;
-        }
-        let role = {
-          username: "",
-          role: ""
-        };
-        let know = [];
-        users.forEach((value, key) => {
-          if (value.username == username) {
-            role.username = value.username;
-            role.role = value.role;
+
+  mongoController.connectToDb().then((db) => db.collection("games").findOne({
+    code: code,
+  }).then((result) => {
+    if (!verifyController.verifyExists(result.users, res)) {
+      return;
+    }
+    const users = result.users;
+    let user = {
+      username: "",
+      role: ""
+    };
+
+    // come-back check this to see if you can do users.username
+    users.forEach((value, key) => {
+      if (value.username == username) {
+        user.username = value.username;
+        user.role = value.role;
+      }
+    });
+
+    let know = [];
+
+    // go through all the players and check which ones you should know
+    switch (user.role) {
+      case "Merlin":
+        users.forEach((player, key) => {
+          if (player.role == "Assassin") {
+            know.push(player);
+          } else if (player.role == "Morgana") {
+            know.push(player);
+          } else if (player.role == "Bad Knight") {
+            know.push(player);
           }
         });
-        switch (role.role) {
-          case "Merlin":
-            users.forEach((value, key) => {
-              if (value.role == "Assassin") {
-                know.push(value);
-              } else if (value.role == "Morgana") {
-                know.push(value);
-              } else if (value.role == "Bad Knight") {
-                know.push(value);
-              }
-            });
-            break;
-          case "Percival":
-            users.forEach((value, key) => {
-              if (value.role == "Merlin" || value.role == "Morgana") {
-                value.role = "Merlin or Morgana";
-                know.push(value);
-              }
-            });
-            break;
-          case "Good Knight":
-            break;
-          case "Assassin":
-            users.forEach((value, key) => {
-              if (value.role == "Morgana") {
-                know.push(value);
-              } else if (value.role == "Bad Knight") {
-                know.push(value);
-              }
-            });
-            break;
-          case "Morgana":
-            users.forEach((value, key) => {
-              if (value.role == "Assassin") {
-                know.push(value);
-              } else if (value.role == "Bad Knight") {
-                know.push(value);
-              }
-            });
-            break;
-          case "Bad Knight":
-            users.forEach((value, key) => {
-              if (value.role == "Assassin") {
-                know.push(value);
-              } else if (value.role == "Morgana") {
-                know.push(value);
-              } else if (value.role == "Bad Knight") {
-                if (value.username != username)
-                  know.push(value);
-              }
-            });
-            break;
-        }
-        res.render("night", {
-          role: role,
-          users: know,
+        break;
+      case "Percival":
+        users.forEach((value, key) => {
+          if (value.role == "Merlin" || value.role == "Morgana") {
+            value.role = "Merlin or Morgana";
+            know.push(value);
+          }
         });
-      }
-    );
-  });
+        break;
+      case "Good Knight":
+        break;
+      case "Assassin":
+        users.forEach((value, key) => {
+          if (value.role == "Morgana") {
+            know.push(value);
+          } else if (value.role == "Bad Knight") {
+            know.push(value);
+          }
+        });
+        break;
+      case "Morgana":
+        users.forEach((value, key) => {
+          if (value.role == "Assassin") {
+            know.push(value);
+          } else if (value.role == "Bad Knight") {
+            know.push(value);
+          }
+        });
+        break;
+      case "Bad Knight":
+        users.forEach((value, key) => {
+          if (value.role == "Assassin") {
+            know.push(value);
+          } else if (value.role == "Morgana") {
+            know.push(value);
+          } else if (value.role == "Bad Knight") {
+            if (value.username != username)
+              know.push(value);
+          }
+        });
+        break;
+    }
+
+    // render night with you and the players you know
+    res.render("night", {
+      role: user,
+      users: know,
+    });
+  }))
 }
 
+// get random roles given some number of players in the game
 function getRoles(numberOfPlayers) {
   let roles = ["Merlin", "Assassin", "Percival", "Morgana", "Good Knight"];
   while (roles.length < numberOfPlayers) {
@@ -159,7 +173,6 @@ function getRoles(numberOfPlayers) {
       roles.push("Bad Knight");
     }
   }
-
 
   for (let i = 0; i < numberOfPlayers; i++) {
     let x = getRandomInt(numberOfPlayers);
@@ -171,9 +184,12 @@ function getRoles(numberOfPlayers) {
   return roles;
 }
 
+// literally just gets a random int given some max int; thanks js
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
+
+
 module.exports = {
   hasStarted,
   assignRoles,
